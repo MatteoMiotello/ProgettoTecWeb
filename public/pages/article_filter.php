3cff96d743a64ca9f2bdfb1b11a56bbce1508af4
gio <calgarogiosue@outlook.com>
2021-05-17 14:47:57 +0200

    aggiunto collegamento selezione articoli per categoria

---
+++ public/pages/article_filter.php
@@ -0,0 +1,48 @@
+<?php
+$CategoryName = $_GET['cat_name'];
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require $_SERVER['DOCUMENT_ROOT'] . '/php/library/TemplateHandler.php' ;
+require_once $_SERVER['DOCUMENT_ROOT'] . '/php/modello.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/php/dBConnection.php';
+require_once $_SERVER['DOCUMENT_ROOT'] .  '/php/library/PreviewArticleBuilder.php';
+
+$dbAccess = new DBAccess();
+$connessioneRiuscita = DBAccess::openDBConnection();
+$connessioneRiuscita = $connessioneRiuscita->getConnection();
+
+$handler = new TemplateHandler();
+$handler->setPageTitle('Categoria'.$CategoryName);
+
+$filePath = $_SERVER['DOCUMENT_ROOT'].'/html/article_filter_nuovo.html';
+
+$handler->setContent(file_get_contents($filePath));
+
+if ($connessioneRiuscita == null)
+    die("Errore nell'apertura del db"); // non si prosegue all'esecuzione della pagina 
+else {
+    $rawArticles = Articolo::getArticoli($CategoryName, $connessioneRiuscita, null);
+    $articlesList = '';
+    if ($rawArticles != null) {
+        foreach ($rawArticles as $articolo) {
+            $articlesList .= (new PreviewArticleBuilder)
+            ->setID($articolo->getId())
+            ->setTitle($articolo->getTitolo())
+            ->setDescription($articolo->getDescrizione())
+            ->setImgPath($articolo->getImgPath())
+            ->setImgAlt($articolo->getAltImg())
+            ->build(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/php/components/articlePreview.phtml'))
+            ;
+        }
+        } else {
+        // messaggio che dice che non ci sono articoli del db
+        $articlesList = "<div>nessun articolo presente</div>";
+    }
+}
+$handler->setParam("{{categoryName}}",$CategoryName);
+$handler->setParam("<listaArticoli />",$articlesList);
+$handler->render(); 
+
+?>
+
