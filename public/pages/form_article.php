3b8f9e48cd28bcbaa929119bf40b7817b24f3565
gio <calgarogiosue@outlook.com>
2021-05-28 17:44:43 +0200

    sistemata scrivi articolo, aggiunti controlli e gestione degli errori in modifica articolo e gestione articolo

--- public/pages/form_article.php
+++ public/pages/form_article.php
@@ -9,7 +9,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] .  '/php/library/CategoryBuilder.php';
 require_once $_SERVER['DOCUMENT_ROOT'] .  '/php/library/ArticleBuilder.php';
 require_once $_SERVER['DOCUMENT_ROOT'] .  '/php/library/Access.php';
 
-$connessioneRiuscita = DBAccess::openDBConnection();
+$connessione = DBAccess::openDBConnection();
 
 $handler = new TemplateHandler();
 $handler->setPageTitle('Form Articolo');
@@ -20,57 +20,70 @@ $filePath = $_SERVER['DOCUMENT_ROOT'] . '/html/form_articolo_nuovo.html';
 $handler->setContent(file_get_contents($filePath));
 $handler->setCurrentRoute('form_articolo');
 
-if (!$connessioneRiuscita)
-    die("Errore nell'apertura del db"); // non si prosegue all'esecuzione della pagina
-else {
-    /**
-     * controllo che l'utente sia loggato 
-    */ 
-    $author = null;
-    $articleContent = '';
-    if(Access::isAuthenticated()) {
-        if(isset($_SESSION['user_id']))
-            $author = Access::getUser();
-    } 
-    /**
-     * controllo se la form e' gia' stata compilata, in tal caso emetto un messaggio di avvenuta operazione con relativo esito
-     */
-    if (isset($_POST['titolo_art']) && isset($_POST['descr_art']) && isset($_POST['testo_art']) && $author) {
-        try{
-            $newArticle = new Articolo(1, $_POST['titolo_art'], $_POST['descr_art'], $_POST['testo_art'], $author->getId(), date('Y-m-d h:i:s'), 0,0,' ', ' ',0 );
-            $result = Articolo::loadNewArticle($newArticle);
-            // controllo che l'operazione sia andata a buon fine
-            if($result)
-                $articleContent = '<article class="articolo_validato" ><p>Articolo inviato correttamente</p></article>';
-            else 
-                $articleContent = '<article class="articolo_non_validato" ><p>Errore nel database, l\'articolo non è stato salvato!</p></article>';
+// controllo che la connessione al db sia andata a buon fine, altrimenti stampo un messaggio di errore
+if (!$connessione) {
+    $handler->setOperationError("Errore nell'apertura del db");
+    return;
+} else
+    $handler->setNoOperation();
 
-        }
-        catch(Exception $e) {
-            // qui sarebbe da settare l'errore direttamente nel template e rendirizzare quello
-            $articleContent = '<article class="articolo_non_validato" ><p>'.$e->getMessage().'</p></article>';
-        }
-        
-    }
-    /**
-     * Nel caso non fosse stata compilata la form allora presento la pagina per poter scrivere l'articolo
-     */
-    else {
-        $categorie = Categoria::getCategorie();
-        if ($categorie != null) {
-            $listaCategoria = '';
-            foreach ($categorie as $singolaCategoria) {
-                $listaCategoria .= (new CategoryBuilder)
-                    ->setName($singolaCategoria->getNome())
-                    ->build(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/php/components/chooseCategoryFormArticle.phtml'));
-            }
-        } else {
-            // messaggio che dice che non ci sono categorie del db
-            $listaCategoria = "<div>nessuna categoria presente</div>";
-        }
-    $articleContent .= (new ArticleBuilder)->build(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/php/components/formArticleContent.phtml'));
-    $handler->setParam("<listaCategorie />", $listaCategoria);
-    $handler->setParam("<formArticleContent />", $articleContent);
-    }
-    $handler->render();
+/**
+ * controllo che l'utente sia loggato 
+ */
+$author = null;
+$articleContent = '';
+if (Access::isAuthenticated()) {
+    if (isset($_SESSION['user_id']))
+        $author = Access::getUser();
+} else {
+    $handler->setOperationError("Non hai eseguito il login! Esegui il login per poter inviarci il tuo elaborato!");
 }
+/**
+ * controllo se la form e' gia' stata compilata, in tal caso emetto un messaggio di avvenuta operazione con relativo esito
+ */
+if (isset($_POST['titolo_art']) && isset($_POST['descr_art']) && isset($_POST['testo_art']) && $author) {
+    try {
+        // provo a caricare l'articolo nel db
+        $articleId = Articolo::getMaxId()+1;
+        $authorId = $author->getId();
+        $newArticle = new Articolo($articleId, $_POST['titolo_art'], $_POST['descr_art'], $_POST['testo_art'], $authorId, date('Y-m-d G:i:s'), '0', '0', 'nada', 'nada', 0);
+        $result = Articolo::loadNewArticle($newArticle);
+
+        // se sono state settate categorie per l'articolo allora le carico nel db
+
+        if(isset($_POST['category'])) {
+            $selectedCat = array();
+            foreach($_POST['category'] as $cat) {
+                $res = Categoria::loadNewCategoryForArticle($cat, $articleId);  
+            }
+        }
+
+        // controllo che l'operazione sia andata a buon fine
+        if ($result) {
+            $handler->setOperationDone("Articolo inviato correttamente");
+        } else {
+            $handler->setOperationError("Errore, i valori inseriti non rispettano gli standard richiesti, l\'articolo non è stato salvato!");
+        }
+    } catch (Exception $e) {
+        $handler->setOperationError($e->getMessage());
+    }
+}
+/**
+ * Presento la pagina per poter scrivere l'articolo
+ */
+$categorie = Categoria::getCategorie();
+if ($categorie != null) {
+    $listaCategoria = '';
+    foreach ($categorie as $singolaCategoria) {
+        $listaCategoria .= (new CategoryBuilder)
+            ->setName($singolaCategoria->getNome())
+            ->build(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/php/components/chooseCategoryFormArticle.phtml'));
+    }
+} else {
+    // messaggio che dice che non ci sono categorie del db
+    $listaCategoria = "<div>nessuna categoria presente</div>";
+}
+$articleContent .= (new ArticleBuilder)->build(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/php/components/formArticleContent.phtml'));
+$handler->setParam("<listaCategorie />", $listaCategoria);
+$handler->setParam("<formArticleContent />", $articleContent);
+$handler->render();
